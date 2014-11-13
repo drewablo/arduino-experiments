339fda4c3fd63e9c78e7d60231b60791fa69485f
drewablo <andrewt77@gmail.com>
2014-11-13 12:09:15 -0600

    Create eyes.h

---
+++ eyes.h
@@ -0,0 +1,118 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+
+
+
+Adafruit_8x8matrix matrix = afruit_8x8matrix()
+
+static const uint8_t PROGMEM // Bitmaps are stored in program memory
+  blinkImg[][8] = {    // Eye animation frames
+  { B00000000,         // Fully open eye
+    B00000000,
+    B11111111,
+    B11111111,
+    B11111111,
+    B11111111,
+    B01111110,
+    B00111100 },
+  { B00000000,
+    B00000000,
+    B01111110,
+    B11111111,
+    B11111111,
+    B11111111,
+    B01111110,
+    B00111100 },
+  { B00000000,
+    B00000000,
+    B00111100,
+    B11111111,
+    B11111111,
+    B11111111,
+    B00111100,
+    B00000000 },
+  { B00000000,
+    B00000000,
+    B00000000,
+    B00111100,
+    B11111111,
+    B01111110,
+    B00011000,
+    B00000000 },
+  { B00000000,         // Fully closed eye
+    B00000000,
+    B00000000,
+    B00000000,
+    B10000001,
+    B01111110,
+    B00000000,
+    B00000000 } },
+
+uint8_t
+  blinkIndex[] = { 1, 2, 3, 4, 3, 2, 1 }, // Blink bitmap sequence
+  blinkCountdown = 100, // Countdown to next blink (in frames)
+  gazeCountdown  =  75, // Countdown to next eye movement
+  gazeFrames     =  50, // Duration of eye movement (smaller = faster)
+
+int8_t
+  eyeX = 3, eyeY = 3,   // Current eye position
+  newX = 3, newY = 3,   // Next eye position
+  dX   = 0, dY   = 0;   // Distance from prior to new position
+
+void setup() {
+
+  // Seed random number generator from an unused analog input:
+  randomSeed(analogRead(A0));
+
+  mantrix.begin(0x70);
+
+}
+
+void loop() {
+
+  // Draw eyeball in current state of blinkyness (no pupil).  Note that
+  // only one eye needs to be drawn.  Because the two eye matrices share
+  // the same address, the same data will be received by both.
+  matrix.clear();
+  // When counting down to the next blink, show the eye in the fully-
+  // open state.  On the last few counts (during the blink), look up
+  // the corresponding bitmap index.
+  matrix.drawBitmap(0, 0,
+    blinkImg[
+      (blinkCountdown < sizeof(blinkIndex)) ? // Currently blinking?
+      blinkIndex[blinkCountdown] :            // Yes, look up bitmap #
+      0                                       // No, show bitmap 0
+    ], 8, 8, LED_ON);
+  // Decrement blink counter.  At end, set random time for next blink.
+  if(--blinkCountdown == 0) blinkCountdown = random(5, 180);
+
+  // Add a pupil (2x2 black square) atop the blinky eyeball bitmap.
+  // Periodically, the pupil moves to a new position...
+  if(--gazeCountdown <= gazeFrames) {
+    // Eyes are in motion - draw pupil at interim position
+    matrix.fillRect(
+      newX - (dX * gazeCountdown / gazeFrames),
+      newY - (dY * gazeCountdown / gazeFrames),
+      2, 2, LED_OFF);
+    if(gazeCountdown == 0) {    // Last frame?
+      eyeX = newX; eyeY = newY; // Yes.  What's new is old, then...
+      do { // Pick random positions until one is within the eye circle
+        newX = random(7); newY = random(6);
+        dX   = newX - 3;  dY   = newY - 3;
+      } while((dX * dX + dY * dY) >= 10);      // Thank you Pythagoras
+      dX            = newX - eyeX;             // Horizontal distance to move
+      dY            = newY - eyeY;             // Vertical distance to move
+      gazeFrames    = random(3, 15);           // Duration of eye movement
+      gazeCountdown = random(gazeFrames, 120); // Count to end of next movement
+    }
+  } else {
+    // Not in motion yet -- draw pupil at current static position
+    matrix.fillRect(eyeX, eyeY, 2, 2, LED_OFF);
+  }
+
+  for(uint8_t i=0; i<1; i++) matrix[i].writeDisplay();
+
+  delay(20); // ~50 FPS
+}
